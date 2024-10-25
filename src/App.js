8c3cecef4fc4bb733b4bcb3d488537d6ec357cb0
David-Good-Enough <124247495+David-Good-Enough@users.noreply.github.com>
2024-10-25 11:28:21 +0200

    {~} fond sur toute la page

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <Router>
-        <div style={{ backgroundColor: 'rgb(66, 71, 94)'}}>
+        <div style={{ backgroundColor: 'rgb(66, 71, 94)', minHeight: '100vh'}}>
         <Header selectedLanguage={language} onLanguageChange={handleLanguageChange} />
         <Routes>
           <Route path="/" element={<Page1 language={language} />} />
